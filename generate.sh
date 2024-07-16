c8a8b398b96f04688ff0a83f409506bbbf9f8fd8
David Newhall II <david@sleepers.pro>
2024-07-15 21:01:40 -0700

    tryt his

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@
 # The generator: https://github.com/Unpackerr/unpackerr/tree/main/init/config
 
 set -e
-GOPROXY=direct
+GOPRIVATE=github.com/Unpackerr/unpackerr
 
 pushd "$(dirname -- "${BASH_SOURCE[0]}")/docs/install"
 go run github.com/Unpackerr/unpackerr/init/config@main docs
